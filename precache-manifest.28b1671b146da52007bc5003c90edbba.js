08f58d518042e61ecfceeee8292accb021733099
BullGator92 <38494083+BullGator92@users.noreply.github.com>
2019-01-23 19:38:16 -0500

    Updates

---
+++ precache-manifest.28b1671b146da52007bc5003c90edbba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "4c489a25db138e3cda95",
+    "url": "/ClickyGame/static/js/main.4c489a25.chunk.js"
+  },
+  {
+    "revision": "441b227c7c9e041cd9b4",
+    "url": "/ClickyGame/static/js/1.441b227c.chunk.js"
+  },
+  {
+    "revision": "4c489a25db138e3cda95",
+    "url": "/ClickyGame/static/css/main.48027d74.chunk.css"
+  },
+  {
+    "revision": "f2eac22fd616bdba1d4687d433fcd346",
+    "url": "/ClickyGame/index.html"
+  }
+];
